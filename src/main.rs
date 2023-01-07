5fcc09a6df1262779db803804596235b5a3a7ed8
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2023-01-07 16:22:51 +0100

    Slow mode.

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,33 @@ struct Args {
 
     #[arg(short, long, default_value_t = 3)]
     pub iterations: usize,
+
+    #[arg(short, long, default_value_t = false)]
+    pub fast: bool,
 }
 
 struct MonteCarloResult {
     pub value: FLOAT,
     pub error: FLOAT,
     pub time: Duration,
+}
+
+fn monte_carlo_slow(samples: u64) -> u64 {
+    let mut rng = rand::thread_rng();
+
+    let mut in_circle = 0;
+
+    for _i in 0..samples {
+        let x: FLOAT = rng.gen();
+        let y: FLOAT = rng.gen();
+
+        let d = x * x + y * y;
+        if d.sqrt() <= (1.0 as FLOAT) {
+            in_circle += 1;
+        }
+    }
+
+    in_circle
 }
 
 fn monte_carlo_fast(samples: u64) -> u64 {
@@ -40,7 +61,7 @@ fn monte_carlo_fast(samples: u64) -> u64 {
     in_circle
 }
 
-fn monte_carlo_iteration(samples: u64, threads: usize) -> MonteCarloResult {
+fn monte_carlo_iteration(fast: bool, samples: u64, threads: usize) -> MonteCarloResult {
     let instant = Instant::now();
 
     let mut handles: Vec<_> = Vec::new();
@@ -48,7 +69,11 @@ fn monte_carlo_iteration(samples: u64, threads: usize) -> MonteCarloResult {
     for _nt in 0..threads {
         let samples = samples.clone();
         handles.push(thread::spawn(move || {
-            monte_carlo_fast(samples)
+            if fast {
+                monte_carlo_fast(samples)
+            } else {
+                monte_carlo_slow(samples)
+            }
         }));
     }
 
@@ -63,7 +88,7 @@ fn main() {
     let args = Args::parse();
 
     for i in 1..=args.iterations {
-        let mc = monte_carlo_iteration(args.samples, args.threads);
+        let mc = monte_carlo_iteration(args.fast, args.samples, args.threads);
         println!("{} / {}: {}", i, args.iterations, mc);
     }
 }
