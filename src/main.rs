adf492c056e6bec1bfc14eb84fc33ccbd775f46d
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2023-01-07 15:38:42 +0100

    Command line parsing and iteration support.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use std::thread;
+use clap::Parser;
 use rand::Rng;
 
 type FLOAT = f64;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = num_cpus::get())]
+    pub threads: usize,
+
+    #[arg(short, long, default_value_t = 10_000)]
+    pub samples: u64,
+
+    #[arg(short, long, default_value_t = 3)]
+    pub iterations: usize,
+}
 
 fn monte_carlo_fast(samples: u64) -> u64 {
     let mut rng = rand::thread_rng();
@@ -21,13 +34,10 @@ fn monte_carlo_fast(samples: u64) -> u64 {
     in_circle
 }
 
-fn main() {
-    let samples = 10000000;
-    let nthreads = num_cpus::get() as u64;
-
+fn monte_carlo_iteration(samples: u64, threads: usize) {
     let mut handles: Vec<_> = Vec::new();
 
-    for _nt in 0..nthreads {
+    for _nt in 0..threads {
         let samples = samples.clone();
         handles.push(thread::spawn(move || {
             monte_carlo_fast(samples)
@@ -36,6 +46,14 @@ fn main() {
 
     let in_circle: u64 = handles.into_iter().map(|handle| handle.join().expect("Failed to join.")).sum();
 
-    let pi: FLOAT = (4.0 as FLOAT) * (in_circle as FLOAT) / ((nthreads * samples) as FLOAT);
+    let pi: FLOAT = (4.0 as FLOAT) * (in_circle as FLOAT) / ((threads as u64 * samples) as FLOAT);
     println!("{}", pi);
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.iterations {
+        monte_carlo_iteration(args.samples, args.threads);
+    }
 }
